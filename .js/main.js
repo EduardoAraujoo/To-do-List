5ccc21651667986d5e394f696e465d5aa92bc7be
Eduardo de Oliveira Araujo <duduoliveira1911@gmail.com>
2023-03-22 17:33:12 -0300

    Bao
    Bao

--- .js/main.js
+++ .js/main.js
@@ -1,2 +1,31 @@
-const form = document.querySelector("#form");
-console.log(form)
+const botaoAdicionar = document.querySelector("#botao");
+var form = document.querySelector("#form");
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var tarefa = colocarTarefa(form);
+    console.log(tarefa);
+
+    adicionaTarefa(tarefa);
+
+})
+
+function colocarTarefa(form) {
+    var tarefa = {
+        tarefa: form.target.elements,
+    }
+    JSON.stringify(tarefa);
+    return tarefa
+}
+function adicionaTarefa(tarefa){
+var tarefaLi = CreateLi(tarefa);
+var ul = document.querySelector("#mae");
+ul.appendChild(tarefaLi);
+}
+function CreateLi(tarefa){
+var tarefaLi = document.createElement("li");
+tarefaLi.classList.add("tarefa")
+return tarefaLi
+}
