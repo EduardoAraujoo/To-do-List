a44a64786fa61247e8c263fe4b358b219ea31786
Eduardo de Oliveira Araujo <duduoliveira1911@gmail.com>
2023-03-28 14:21:39 -0300

    OK

--- .js/validacao.js
+++ .js/validacao.js
@@ -1,10 +1,19 @@
 function createLiWithTask(form) {
     var ul = document.getElementById("ul");
+    
     var tarefaLi = document.createElement("li");
+    
     tarefaLi.classList.add("invisivel");
     tarefaLi.classList.add("tarefa");
     tarefaLi.classList.add("tipo-de-texto");
     tarefaLi.textContent = form.principal.value;
+    
+    
+    var botaoExcluir = document.createElement("button");
+    botaoExcluir.classList.add("excluir");
+    botaoExcluir.innerHTML = ""
+    
+    tarefaLi.appendChild(botaoExcluir);
     ul.appendChild(tarefaLi);
-    //tarefaLi.appendChild(botaoDeletador);
+    
 }
