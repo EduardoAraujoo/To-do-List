1d1bbc6873aabc7e74c3f3bb6e76ffd9ad3d321e
Eduardo de Oliveira Araujo <duduoliveira1911@gmail.com>
2023-03-28 18:24:03 -0300

    ALELUIA
    ALELUIA

--- .js/delete.js
+++ .js/delete.js
@@ -1,11 +1,17 @@
 var botaoDelete = document.querySelectorAll('.excluir');
 
-botaoDelete.forEach(function(){
-    addEventListener("click", function (event) {
+for (var i = 0; i < botaoDelete.length; i++){
+    botaoDelete[i].addEventListener("click", function (event) {
         var alvo = event.target;
         var paiAlvo = alvo.parentNode;
         paiAlvo.remove();
-    })});
+    })
+}
 
 
 
+// botaoDelete.addEventListener("click", function (event) {
+//     var alvo = event.target;
+//     var paiAlvo = alvo.parentNode;
+//     paiAlvo.remove();
+// })
