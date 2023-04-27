7fa2d3bde56574818ed1f650868e37c316796e94
Eduardo de Oliveira Araujo <duduoliveira1911@gmail.com>
2023-04-27 20:33:40 -0300

    bao

--- .js/theme.js
+++ .js/theme.js
@@ -1,6 +1,22 @@
-var icon = document.querySelector(".lua");
 var body = document.querySelector("body");
-icon.addEventListener("click", () =>{
-body.classList.toggle("dark-theme");
-})
+var sun = document.querySelector(".gg-sun");
+var moon = document.querySelector(".gg-moon");
+var buttonToggle = document.querySelector("")
 
+var i = document.getElementById("trocar");
+
+i.addEventListener("click", () => {
+    body.classList.toggle("dark-theme");
+
+    if (i.classList.contains("gg-moon") == true) {
+        i.classList.remove("gg-moon");
+        i.classList.add("gg-sun");
+        
+    } else if (i.classList.contains("gg-sun") == true) {
+        i.classList.add("gg-moon");
+        i.classList.remove("gg-sun");
+    }
+    else {
+        console.log("deu erro");
+    }
+});
